fe31abfc12c3c12fb071ed9b5fac598f981f9c94
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-19 02:29:09 -0500

    x

--- src/models/Servicios.js
+++ src/models/Servicios.js
@@ -4,9 +4,9 @@ const modelserv = new mongoose.Schema({
     ServicioID: { type: Number, required: true, unique: true },
     Nombre: { type: String, required: true },
     Descripción: { type: String, required: true },
-    Precio: { type: Number, required: true },
+    Precio: { type: Number, required: true }, 
     Tipo: { type: String, required: true },
-    Duracionn: { type: Number, required: true },  // Mantén Duracionn consistente
+    Duracionn: { type: Number, required: true }, 
 }, {
     timestamps: true
 });
