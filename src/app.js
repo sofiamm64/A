bf0bf05c0346bac40e655f3c276767501802d564
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-18 21:52:01 -0500

    next

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import morgan from 'morgan'
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';  // Cambiar 'require' a 'import'
 import autorouter from './routers/auto_routers.js';
 
-const cors = require('cors');
 const app = express();
+
 app.use(morgan('dev'));
 app.use(express.json());
+
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
-}),
-app.use(autorouter)
-);
+}));
+
+app.use(autorouter);
 
 export default app;
