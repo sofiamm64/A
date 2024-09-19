812cd65803d372d054bdf5f1274a6e31dccd07e8
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-18 21:49:39 -0500

    next

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import app from './app.js';
 import {connectDB} from './db.js'
 
 connectDB();
-app.listen(process.env.PORT, () => {
-    console.log(`Abierto en el puerto ${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Abierto en el puerto ${port}`);
 });
+
