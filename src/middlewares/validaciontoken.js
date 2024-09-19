d2e03d5f23f6a2a517afebce77cc8630f5d9927f
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-18 23:03:38 -0500

    next

--- src/middlewares/validaciontoken.js
+++ src/middlewares/validaciontoken.js
@@ -3,12 +3,19 @@ import { tokensecreto } from '../config.js';
 
 export const autenticacion = (req, res, next) => {
   const authHeader = req.headers['authorization'];
+  console.log('Authorization Header:', authHeader); // Depura el encabezado
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) return res.status(401).json({ mensaje: "No autorizado" });
+  if (!token) {
+    console.log('No token provided');
+    return res.status(401).json({ mensaje: "No autorizado" });
+  }
 
   jwt.verify(token, tokensecreto, (err, user) => {
-    if (err) return res.status(403).json({ mensaje: "Token inválido" }); 
+    if (err) {
+      console.log('Token verification failed:', err);
+      return res.status(403).json({ mensaje: "Token inválido" }); 
+    }
     req.user = user; 
     next(); 
   });
