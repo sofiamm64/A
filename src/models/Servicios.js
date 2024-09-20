7392cbda2e0a9c8af7c2206cb500bfc67aa635f3
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-19 20:20:33 -0500

    servicio

--- src/models/Servicios.js
+++ src/models/Servicios.js
@@ -1,22 +1,16 @@
-import mongoose from 'mongoose';
-
 const modelserv = new mongoose.Schema({
     ProductoServicioID: { type: Number, required: true, unique: true },
     Nombre: { type: String, required: true },
     Descripción: { type: String, required: true },
-    Precio: { type: Number, required: true, default: 0 }, 
-    Tipo: { 
-        type: String, 
-        required: true, 
-        enum: ['Producto', 'Servicio'], 
-        default: 'Producto'
+    Precio: { type: Number, required: true, default: 0 },
+    Tipo: {
+      type: String,
+      required: true,
+      enum: ['Producto', 'Servicio'],
+      default: 'Producto'
     },
-    Duracion: { 
-        type: Number, 
-        default: 0 
-    },
-}, {
+    Duracion: { type: Number, default: 0 }
+  }, {
     timestamps: true,
-});
-
-export default mongoose.model('Servicio', modelserv);
+  });
+  
