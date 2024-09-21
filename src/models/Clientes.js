a15f5a074e491dd5ca9b862469f3f7d8270af1cf
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-21 13:03:55 -0500

    clientes

--- src/models/Clientes.js
+++ src/models/Clientes.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const modelclie = new mongoose.Schema({
-    clienteID: { type: Number, unique: true, required: true }, 
+  clienteID: { type: Number, unique: true, required: true }, 
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
   email: { type: String, required: true },
