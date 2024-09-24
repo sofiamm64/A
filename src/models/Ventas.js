57a1700606777caa9fd84c8bddad623309ddff39
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-24 09:23:23 -0500

    venta

--- src/models/Ventas.js
+++ src/models/Ventas.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const modelvent = new mongoose.Schema({
   VentaID: { type: Number, required: true, unique: true },
-  ClienteID: { type: String, required: true },
+  ClienteID: { type: Number, required: true },
   ServicioID: { type: String, required: true },
   FechaVenta: { type: Date, required: true },
   Cantidad: { type: Number, required: true, default: 0 },
