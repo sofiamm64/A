90da25f3169de91c6b950d89cd7230bed02864f7
sofiamm64 <sofiamadrigal0064@gmail.com>
2024-09-24 08:13:08 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(cors({
   origin: 'http://localhost:5173',
   methods: 'GET,POST,PUT,DELETE',
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
 app.get('/', (req, res) => {
